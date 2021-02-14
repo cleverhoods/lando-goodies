9b4a673f6348f3006d31eb76d6eed0fb73b46fe5
Gábor Mészáros <gabor@cleverhoods.com>
2021-02-14 18:17:41 +0100

    Move variable.

--- local/scripts/clean.sh
+++ local/scripts/clean.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 source "$(dirname "$0")/common.sh"
 
-PROJECT_ROOT="$(get_project_root)"
-
 log "\e[36m--------- Cleaning downloaded and built resources ---------\e[39m"
 
 log "Go to the project root."
+PROJECT_ROOT="$(get_project_root)"
 cd "${PROJECT_ROOT}" || exit 1;
 
 TODELETE=("vendor" "web/core")
